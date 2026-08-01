6f96d2c71d9f78a328d447df14d47a6e359e00a4
HenryAlman <95183441+HenryAlman@users.noreply.github.com>
2026-08-01 21:57:22 +0100

    Update script to use absolute path for Python script

--- run_mujoco_array.sh
+++ run_mujoco_array.sh
@@ -30,6 +30,6 @@ export NUMEXPR_NUM_THREADS=1
 export VECLIB_MAXIMUM_THREADS=1
 export BLIS_NUM_THREADS=1
 
-python3 PythonDissertation/mapelites_mujoco_test_cpg_3.py $ARGS
+python3 /users/40795510/sharedscratch/dissertation/mapelites_mujoco_test_cpg_3.py $ARGS
 
 conda deactivate
